93d0a5ef2b8c0dfab032ac4b30d361fc314dbc02
bmxp <bernd.meiners@mail.de>
2020-04-13 16:04:20 +0200

    reverted all changes in travis to develop

--- .travis.sh
+++ .travis.sh
@@ -31,7 +31,7 @@ LINKS="smarthome/plugins/plugins"
 
 # Get the current repository which is processed
 REPOSITORY="$(basename $TRAVIS_REPO_SLUG)"
-OWNER_NAME="$(dirname  $TRAVIS_REPO_SLUG)"
+REPO_OWNER="$(dirname  $TRAVIS_REPO_SLUG)"
 REPOSITORY_ORIGIN="$REPOSITORY_ORIGIN"
 
 # Find out on which branch to work
@@ -41,45 +41,27 @@ REPOSITORY_ORIGIN="$REPOSITORY_ORIGIN"
 #  REPOSITORY_BRANCH="develop"
 #fi
 
-# we might have more branches than develop or master so use the branch
-# that Travis has preset by gitub
 REPOSITORY_BRANCH=$TRAVIS_BRANCH
-
+echo -e "Current branch for $REPOSITORY is $REPOSITORY_BRANCH"
 
 #######################################################################
 # 1. Checkout all repositories
 
-echo -e "travis_fold:start:Checkout\nChecking out repositories with $REPOSITORY_BRANCH branch"
+echo -e "travis_fold:start:Checkout\nChecking out additional repositories with $REPOSITORY_BRANCH branch"
 
 # Change to root directory since script is started in checkout
 cd $TRAVIS_BUILD_DIR/..
 
 # Check out other repositories with develop version
-##for REPO in $REPOSITORIES ; do
-##  if [ "$REPO" != "$REPOSITORY_ORIGIN" ] ; then
-##    echo "Checking out $REPO ..."
-##    #git clone https://github.com/$OWNER_NAME/$REPO.git $REPO
-##    git clone https://github.com/$OWNER_NAME/$REPO.git $REPO
-##    cd $REPO
-##    git checkout $REPOSITORY_BRANCH
-##    cd ..
-##  fi
-##done
-REPO = smarthome
-echo "Checking out $REPO ..."
-git clone https://github.com/$OWNER_NAME/$REPO.git $REPO
-cd $REPO
-git checkout $REPOSITORY_BRANCH
-echo "Checking out $REPO ... done"
-
-REPO = plugins
-echo "Checking out $REPO ..."
-git clone https://github.com/$OWNER_NAME/$REPO.git $REPO
-echo "Checking out $REPO ... done"
-cd $REPO
-git checkout $REPOSITORY_BRANCH
-echo "Checking out $REPO ... done"
-cd ..
+for REPO in $REPOSITORIES ; do
+  if [ "$REPO" != "$REPOSITORY_ORIGIN" ] ; then
+    echo "Checking out $REPO ..."
+    git clone https://github.com/$REPO_OWNER/$REPO.git $REPO
+    cd $REPO
+    git checkout $REPOSITORY_BRANCH
+    cd ..
+  fi
+done
 
 echo "travis_fold:end:Checkout"
 
@@ -89,21 +71,19 @@ echo "travis_fold:end:Checkout"
 
 echo -e "travis_fold:start:Links\nCreating symlinks"
 
-# LINKS="smarthome/plugins/plugins"
-
 # Create symlinks in core repository
-#for LINK in $LINKS ; do
-#  TARGET=$(dirname "$LINK")             # TARGET = smarthome/plugins
-#  TARGET_REPO=$(dirname "$TARGET")      # TARGET_REPO = smarthome
-#  TARGET_DIR=$(basename "$TARGET")      # TARGET_DIR = plugins
-#  SOURCE_REPO=$(basename "$LINK")       # SOURCE_REPO = plugins
-#
-#  echo "Create link from $SOURCE_REPO to $TARGET_REPO/$TARGET_DIR ..."
-#  cd $TARGET_REPO                       # cd smarthome
-#  rm -rf $TARGET_DIR                    # rm -rf plugins
-#  ln -s ../$SOURCE_REPO $TARGET_DIR     # symbolic link from plugins to ../plugins
-#  cd ..
-#done
+for LINK in $LINKS ; do
+  TARGET=$(dirname "$LINK")
+  TARGET_REPO=$(dirname "$TARGET")
+  TARGET_DIR=$(basename "$TARGET")
+  SOURCE_REPO=$(basename "$LINK")
+
+  echo "Create link from $SOURCE_REPO to $TARGET_REPO/$TARGET_DIR ..."
+  cd $TARGET_REPO
+  rm -rf $TARGET_DIR
+  ln -s ../$SOURCE_REPO $TARGET_DIR
+  cd ..
+done
 
 echo "travis_fold:end:Links"
 
@@ -114,7 +94,8 @@ echo "travis_fold:end:Links"
 echo -e "travis_fold:start:Suite\nRunning suite"
 
 cd smarthome
-tox || exit 1
+#tox || exit 1
+echo "would run tox now"
 cd ..
 
 echo "travis_fold:end:Suite"
@@ -123,10 +104,11 @@ echo "travis_fold:end:Suite"
 #######################################################################
 # 4. Docs
 
-#echo -e "travis_fold:start:Docs\nBuilding documentation"
+echo -e "travis_fold:start:Docs\nBuilding documentation"
 
-#cd smarthome/doc
+cd smarthome/doc
 #yes | bash build_doc.sh
-#cd ../..
+echo "would build docs here"
+cd ../..
 
-#echo "travis_fold:end:Docs"
+echo "travis_fold:end:Docs"
